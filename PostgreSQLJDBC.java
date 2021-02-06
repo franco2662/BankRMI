a1fb90c52607bb7943bacfac9d197e03c843ba79
Franco Marino <femarino.16@est.ucab.edu.ve>
2021-02-06 01:19:35 -0400

    Update PostgreSQLJDBC.java
    Progreso con la conexión a BD

--- PostgreSQLJDBC.java
+++ PostgreSQLJDBC.java
@@ -1,5 +1,8 @@
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.Statement;
 
 public class PostgreSQLJDBC {
    public static void main(String args[]) {
@@ -9,10 +12,21 @@ public class PostgreSQLJDBC {
          String password = "1234";
       System.err.println("hola");
       try {
+         //Class.forName("org.postgresql.Driver");
          connection = DriverManager.getConnection(jdbcURL,username,password);
+         Statement stmt = null;
+         stmt = connection.createStatement();
+        
+         ResultSet rs = stmt.executeQuery( "SELECT * FROM Operation" ); 
+
+            while (rs.next()) {
+                  System.out.print(rs.getString(1));
+            }
+
          
       } catch (Exception e) {
          System.out.println(e.getMessage());
+         System.out.println("fallo");
       }      
       
       
